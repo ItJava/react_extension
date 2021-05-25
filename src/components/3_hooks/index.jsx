d210f2a044a217bfe801efafa11c023271bce4c3
itjavawfc@163.com <itjavawfc@163.com>
2021-05-25 08:59:29 +0800

    Ref Hook 基础使用，对于某个容器专用，存储一些值

--- src/components/3_hooks/index.jsx
+++ src/components/3_hooks/index.jsx
@@ -6,8 +6,16 @@ export default class Demo extends Component {
 
     state={count:0}
 
+    myRef=React.createRef()
+
     add=()=>{
         this.setState(state=>({count:state.count+1}))
+    }
+
+
+    show=()=>{
+        //this.setState(state=>({count:state.count+1}))
+        alert(this.myRef.current.value)
     }
 
 
@@ -33,9 +41,11 @@ export default class Demo extends Component {
     render() {
         return (
             <div>
+                <input type='text' ref={this.myRef} />
                 <h2>当前求和为{this.state.count}</h2>
                 <button onClick={this.add}>点我加一</button>
                 <button onClick={this.unmount}>卸载组件</button>
+                <button onClick={this.show}>显示数据</button>
             </div>
         );
     }
@@ -49,13 +59,22 @@ function Demo(){
     const [count,setCount]=React.useState(0)
     const [name,setNmae]=React.useState('wangfangchen')
 
-  function  add(){
+    const   myRef=React.createRef()
+
+
+    function  add(){
       // setCount(count+1)  // 第一种写法：参数为费函数值，直接指定新的状态值，内部勇气覆盖原来的值
       setCount(count=> count+1)                     // 参数为函数，接收原来的状态值，返回新的状态值，内部用其覆盖原来的值
     }
 
   function  changeName(){
       setNmae('jack')  // 参数为函数，接收原来的状态值，返回新的状态值，内部用其覆盖原来的值
+    }
+
+
+    function  show(){
+        //this.setState(state=>({count:state.count+1}))
+        alert(myRef.current.value)
     }
 
 
@@ -86,11 +105,15 @@ function Demo(){
 
     return (
         <div>
+            <input type='text' ref={myRef} />
+
             <h2>当前求和为{count}</h2>
             <h2>我的名字是:{name}</h2>
             <button onClick={add}>点我加一</button>
             <button onClick={changeName}>点我改名</button>
             <button onClick={unmount}>卸载组件</button>
+            <button onClick={show}>显示数据</button>
+
         </div>
 
     )
